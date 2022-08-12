4d5b08f7b7740d210bd69bb9a78e7e50c5cb72c4
sfd-mhrana <sfd.mhrana@gmail.com>
2022-08-13 03:21:42 +0600

    Manually Subscription

--- src/app/rxcounter/counter-output/counter-output.component.ts
+++ src/app/rxcounter/counter-output/counter-output.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -7,15 +8,26 @@ import { CounterState } from '../state/counter.state';
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit {
-  counter:number=0;
+export class CounterOutputComponent implements OnInit , OnDestroy{
+  counter: number = 0;
+  counterSubscription: Subscription;
 
-  constructor(private store:Store<{counter:CounterState}>) { }
+  constructor(private store: Store<{ counter: CounterState }>) {
+      
+  }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe(data=>{
-      this.counter=data.counter;
-    })
+    this.counterSubscription=this.store
+    .select('counter')
+      .subscribe(data => {
+        this.counter = data.counter;
+      })
+  }
+
+  ngOnDestroy(): void {
+    if(this.counterSubscription){
+      this.counterSubscription.unsubscribe();
+    }
   }
 
 }
