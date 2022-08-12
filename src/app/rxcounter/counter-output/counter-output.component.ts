b6da28c29c4aaeecc55f643fae29545b09b83f83
sfd-mhrana <sfd.mhrana@gmail.com>
2022-08-13 04:11:27 +0600

    Auto Suscribe with Observable

--- src/app/rxcounter/counter-output/counter-output.component.ts
+++ src/app/rxcounter/counter-output/counter-output.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CounterState } from '../state/counter.state';
 
 @Component({
@@ -8,26 +8,33 @@ import { CounterState } from '../state/counter.state';
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit , OnDestroy{
-  counter: number = 0;
-  counterSubscription: Subscription;
+export class CounterOutputComponent implements OnInit, OnDestroy {
+  // counter: number = 0;
+  // counterSubscription: Subscription;
 
-  constructor(private store: Store<{ counter: CounterState }>) {
-      
+
+  counter$: Observable<{counter: number}>
+  
+
+  constructor(private store: Store<{counter: CounterState}>) {
+
   }
 
   ngOnInit(): void {
-    this.counterSubscription=this.store
-    .select('counter')
-      .subscribe(data => {
-        this.counter = data.counter;
-      })
+    // this.counterSubscription = this.store
+    //   .select('counter')
+    //   .subscribe(data => {
+    //     this.counter = data.counter;
+    //   });
+
+    this.counter$ = this.store.select('counter')
+    
   }
 
   ngOnDestroy(): void {
-    if(this.counterSubscription){
-      this.counterSubscription.unsubscribe();
-    }
+    // if (this.counterSubscription) {
+    //   this.counterSubscription.unsubscribe();
+    // }
   }
 
 }
